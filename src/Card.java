2692e4bdddedc53b24eb7bc464fae7d2dc0ba934
bhautiksojitra <48924148+bhautiksojitra@users.noreply.github.com>
2021-03-23 16:12:57 -0500

    Initial commit

---
+++ src/Card.java
@@ -0,0 +1,12 @@
+public class Card {
+
+    private String typeCard;
+    private int value;
+
+    public Card(String theType , int theValue)
+    {
+        typeCard = theType;
+        value = theValue;
+    }
+
+}
